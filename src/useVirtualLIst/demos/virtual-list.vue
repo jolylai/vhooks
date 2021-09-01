6e1202b1d3102638c766d1d5ba72fac349d0d6f7
jolylai <jolylai163@163.com>
2021-09-01 18:29:12 +0800

    feat: useVirtualList

--- src/useVirtualLIst/demos/virtual-list.vue
+++ src/useVirtualLIst/demos/virtual-list.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>ddd</div>
+  <div v-bind="containerProps" :style="{ height: '200px' }">
+    <ul v-bind="wrapperProps" :style="{ 'list-style': 'none' }">
+      <li v-for="{ data } in list" :style="{ height: '30px' }">
+        {{ data }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 import { useVirtualList } from "usevhooks";
-console.log("useVirtualList: ", useVirtualList);
-const list = Array.from({ length: 10000 }).keys();
+
+const dataSource = Array.from(Array(99999).keys());
+
+const { list, containerProps, wrapperProps } = useVirtualList(dataSource, {
+  itemHeight: 30,
+  overscan: 5,
+});
 </script>
--- src/useVirtualLIst/index.ts
+++ src/useVirtualLIst/index.ts
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { computed, reactive, ref, watch, watchEffect } from "vue";
 
 export interface OptionType {
   itemHeight?: number;
@@ -7,10 +7,97 @@ export interface OptionType {
 
 const useVirtualList = <T>(originalList: T[], options: OptionType) => {
   const list = ref();
+  const { itemHeight, overscan = 5 } = options;
 
-  const containerProps = ref({});
+  if (!itemHeight) {
+    console.warn("please enter a valid itemHeight");
+  }
 
-  const wrapperProps = ref([]);
+  const state = reactive({
+    start: 0,
+    end: 10,
+  });
+
+  watchEffect(() => {
+    const { start, end } = state;
+
+    list.value = originalList
+      .slice(start, end)
+      .map((data, index) => ({ data, index: index + start }));
+  });
+
+  const totalHeight = computed(() => {
+    const { length } = originalList;
+    return itemHeight * length;
+  });
+
+  const offsetTop = computed(() => {
+    console.log("offsetTop: ");
+    const start = state.start;
+
+    return getDistanceTop(start);
+  });
+
+  const containerRef = ref<HTMLElement>();
+
+  // 获取被卷起项的数量
+  const getOffset = (scrollTop: number) => {
+    if (typeof itemHeight === "number") {
+      return Math.floor(scrollTop / itemHeight) + 1;
+    }
+
+    // todo 动态计算
+  };
+
+  // 获取容器能展示几个
+  const getViewCapacity = (containerHeight: number) => {
+    return Math.ceil(containerHeight / itemHeight);
+  };
+
+  const calculateRange = () => {
+    const element = containerRef.value;
+
+    if (element) {
+      const offset = getOffset(element.scrollTop);
+      const viewCapacity = getViewCapacity(element.clientHeight);
+
+      const fromIndex = offset - overscan;
+      const toIndex = offset + viewCapacity + overscan;
+
+      state.start = Math.max(fromIndex, 0);
+      state.end = toIndex > originalList.length ? originalList.length : toIndex;
+    }
+  };
+
+  const onContainerScroll = (e) => {
+    calculateRange();
+  };
+
+  const getDistanceTop = (index: number) => {
+    return index * itemHeight;
+  };
+
+  const containerProps = ref({
+    ref: (el) => {
+      containerRef.value = el;
+      el.addEventListener("scroll", onContainerScroll, false);
+    },
+    style: { overflowY: "auto" },
+  });
+
+  const wrapperProps = computed(() => {
+    const { start } = state;
+
+    const offsetTop = getDistanceTop(start);
+
+    return {
+      style: {
+        width: "100%",
+        height: totalHeight.value - offsetTop + "px",
+        marginTop: offsetTop + "px",
+      },
+    };
+  });
 
   return {
     list,
